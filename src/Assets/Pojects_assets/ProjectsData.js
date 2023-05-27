374ae23d02344633d3c0b5b7e5028d56550f5993
Akash <akashbytes@gmail.com>
2023-05-27 17:35:27 +0530

    project assets

---
+++ src/Assets/Pojects_assets/ProjectsData.js
@@ -0,0 +1,67 @@
+
+// import Foodies from "./Foodies.jpg"
+import Foodies from "./Swigyy.gif"
+
+
+export const ProjectHelper = [
+    {
+        id:"10P1" ,
+        imgs:Foodies,
+        name:"Foodies(Swiggy Clone )",
+        V_URL: "",
+        G_URL : "",
+    } ,
+    // {
+    //     id:"10P2" ,
+    //     imgs:ImageName ,
+    //     name:"You Tube (You Tube Clone)" ,
+    //     V_URL: "",
+    //     G_URL : "",
+    // } ,
+  
+    // {
+    //     id:"10C" ,
+    //     imgs:ImageName ,
+    //     name:"" ,
+    //     V_URL: "",
+    //     G_URL : "",
+    // } ,
+
+    // {
+    //     id:"10D" ,
+    //      imgs:ImageName ,
+        // name:"" ,
+        // V_URL: "",
+        // G_URL : "",
+    // } ,
+
+    // {
+    //     id:"10E" ,
+    //     imgs:react ,
+    //     name:"REACT" ,
+    //     url : "https://react.dev/"
+    // } ,
+  
+    // {
+    //     id:"10F" ,
+    //      imgs:ImageName ,
+        // name:"" ,
+        // V_URL: "",
+        // G_URL : "",
+    // } ,
+    // {
+    //     id:"10G" ,
+    //      imgs:ImageName ,
+        // name:"" ,
+        // V_URL: "",
+        // G_URL : "",
+    // } ,
+    // {
+    //     id:"10H" ,
+    //      imgs:ImageName ,
+        // name:"" ,
+        // V_URL: "",
+        // G_URL : "",
+    // } ,
+
+]
---
+++ src/Assets/Skills_assets/skillsData.js
@@ -0,0 +1,67 @@
+import html from "./html.png"
+import Css from "./css.png"
+import bootstrap from "./Bootstrap.png"
+import javascript from "./javascript.png"
+import react from "./react.png"
+import git from "./gitHub.png"
+import tailwind from "./tailwindCss.png"
+import next from "./Nextjs.png"
+
+
+export const skillsName = [
+        {
+            id:"10A" ,
+            imgs:html ,
+            name:"HTML",
+            url : "https://html.com/html5/"
+        } ,
+        {
+            id:"10B" ,
+            imgs:Css ,
+            name:"CSS" ,
+            url : "https://www.w3.org/TR/CSS/#css"
+        } ,
+      
+        {
+            id:"10C" ,
+            imgs:javascript ,
+            name:"JAVASCRIPT" ,
+            url : "https://developer.mozilla.org/en-US/docs/Web/JavaScript"
+        } ,
+
+        {
+            id:"10D" ,
+            imgs:bootstrap ,
+            name:"BOOTSRAP" ,
+            url : "https://getbootstrap.com/docs/5.0/getting-started/introduction/"
+        } ,
+    
+        {
+            id:"10E" ,
+            imgs:react ,
+            name:"REACT" ,
+            url : "https://react.dev/"
+        } ,
+      
+        {
+            id:"10F" ,
+            imgs:git ,
+            name:"GIT" ,
+            url : "https://git-scm.com/"
+        } ,
+        {
+            id:"10G" ,
+            imgs:tailwind ,
+            name:"TAILWIND" ,
+            url : "https://tailwindcss.com/"
+        } ,
+        {
+            id:"10H" ,
+            imgs:next ,
+            name:"NEXT" ,
+            url : "https://nextjs.org/"
+        } ,
+    
+    ]
+    
+    
