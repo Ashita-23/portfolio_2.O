3c73be8acd66292b921fbc31d44022b312ab5ad6
Akash <akashbytes@gmail.com>
2023-06-07 22:57:24 +0530

    edited some old code with new code

--- src/Components/BodyComponents/ProjectComponents/ProjectCards.js
+++ src/Components/BodyComponents/ProjectComponents/ProjectCards.js
@@ -3,14 +3,15 @@ import "./ProjectCards.css"
 import "./ProjectsCardsMedia.css"
 
 const ProjectCards = ({ProjectHelper}) =>{
-    console.log(ProjectHelper)
+    // console.log(ProjectHelper)
     return(<>
   <div className="pro-card-outer">
-    {/* <figure > */}
-  <img src={ProjectHelper[0].imgs} className="pro-img" />
-    {/* </figure> */}
+  <figure className="figure-box">
+  <img src={ProjectHelper.imgs} className="pro-img" />
+  </figure>
+
     <div className="pro-deatil-box">
-        <span className="project-name">{ProjectHelper[0].name}</span>
+        <span className="project-name">{ProjectHelper.name}</span>
         <span className="discreption">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Impedit quasi, magni molestiae debitis odio natus!</span>
         <div className="pro-btn-group">
         <span className="link-btn"><a href="https://github.com/Ashita-23/Swiggy_Clone_Repo" title="Go live" target="blank"><i className="fa-solid fa-link"></i></a></span>
