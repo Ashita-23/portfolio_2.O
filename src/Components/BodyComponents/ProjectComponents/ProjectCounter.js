3290954e6b2428d3a4e26f4610e39c4add0d79f3
Akash <akashbytes@gmail.com>
2023-06-07 22:59:08 +0530

    added map for rendring  projectcards

--- src/Components/BodyComponents/ProjectComponents/ProjectCounter.js
+++ src/Components/BodyComponents/ProjectComponents/ProjectCounter.js
@@ -4,13 +4,13 @@ import { ProjectHelper } from "../../../Assets/Pojects_assets/ProjectsData"
 import ProjectCards from "./ProjectCards"
 
 const ProjectCounter = () =>{
-    console.log(ProjectHelper)
+    // console.log(ProjectHelper)
     return(<>
         <div className="project-counter" id="project">
         <div className="project-inner-box">
             <h1 className="pro-heading"> Projects</h1>
             <div className="project-display">
-                <ProjectCards ProjectHelper={ProjectHelper}/>
+                {ProjectHelper.map((value)=><ProjectCards ProjectHelper={value} key={value.id} />)}
             </div>
             </div>
         </div>
