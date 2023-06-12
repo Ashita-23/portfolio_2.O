a82d62ea115806e17945af5056d1900a6fec79a2
Akash <akashbytes@gmail.com>
2023-06-12 16:00:58 +0530

    created User Online and  Offline status fu

--- src/Components/HeaderComponents/NavigationComponets/Navigation.js
+++ src/Components/HeaderComponents/NavigationComponets/Navigation.js
@@ -1,19 +1,18 @@
 import "./Navigation.css"
 import "./NavigationMedia.css"
-
-// import BG_Animation from "../../Assets/Animation/BG_Animaton"
+import { IsUserOnline } from "../../../Assets/ProjectFunctions/IsUserOnline"
 
 const Navigation = () => {
+const IsOnline = IsUserOnline()
+if(!IsOnline) return alert("your internet connection is gone!")
     return(<>
      <nav className="nav-outer">
-        {/* <BG_Animation/> */}
         <ul className="link-list">
             <li><a href="#/"><i className="fa-solid fa-house-user"></i></a></li>
             <li><a href="#about"><i className="fa-solid fa-circle-info"></i></a></li>
             <li><a href="#skills"><i className="fa-solid fa-code"></i></a></li>
             <li><a href="#project"><i className="fa-regular fa-folder-open"></i></a></li>
             <li><a href="#contact"><i className="fa-regular fa-paper-plane"></i></a></li>
-            
             </ul>    
      </nav>
     </>)
