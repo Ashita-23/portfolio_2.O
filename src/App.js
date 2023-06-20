a7b5f393564280469ac6b1d199e807e4d3bdf4b7
Akash <akashbytes@gmail.com>
2023-06-20 10:52:28 +0530

    added all components and UserStatus fn with component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 
 import './App.css';
-// import Navigation from './Components/HeaderComponents/NavigationComponets/Navigation';
-// import Home from './Components/HeaderComponets/HomeComponent/Home';
+import { IsUserOnline } from "./Assets/ProjectFunctions/IsUserOnline"
+import NetworkStatus from "./Components/HeaderComponents/OnlineStatus/NetworkStatus"
+import Navigation from "./Components/HeaderComponents/NavigationComponets/Navigation"
+import Home from "./Components/HeaderComponents/HomeComponent/Home"
+import About from "./Components/BodyComponents/AboutComponents/About"
+import SkillCounter from "./Components/BodyComponents/SkillComponents/SkillCounter"
+import ProjectCounter from "./Components/BodyComponents/ProjectComponents/ProjectCounter"
+import ContactForm from "./Components/BodyComponents/ContactComponents/ContactForm"
+import Footer from "./Components/FooterComponents/Footer"
 
 function App() {
+  const IsOnline = IsUserOnline()
+if(!IsOnline) return <NetworkStatus></NetworkStatus>
   return ( <>
-    {/* <Navigation></Navigation> */}
-    {/* <Home></Home> */}
+<Navigation></Navigation>
+<Home></Home>
+<About></About>
+<SkillCounter></SkillCounter>
+<ProjectCounter></ProjectCounter>
+<ContactForm></ContactForm>
+<Footer></Footer>
+
   </> );
 }
 
